f827375bcb040425bbed321fc3e7e0a15e57e666
= <tsmit291@icloud.com>
2016-03-02 11:07:55 -0700

    info added to pirates seed file

--- 01-angular-express-postgres/pirates/seeds/pirates.js
+++ 01-angular-express-postgres/pirates/seeds/pirates.js
@@ -1,12 +1,24 @@
-
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('pirates').del(),
 
-    // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('pirates').insert({
+      name: 'Anne Bonney',
+      poison: 'Rum',
+      accessory: 'hot temper',
+      image_url: 'http://2.bp.blogspot.com/-WSKMEi_MH5U/U-AdUnMNuvI/AAAAAAAABI8/fv0BXLICx8c/s1600/Anne_Bonny_color.jpg'
+    }),
+    knex('pirates').insert({
+      name: 'Captain Jack Sparrow',
+      poison: 'Whiskey',
+      accessory: 'intoxication',
+      image_url: 'http://jamaicanechoes.com/wp-content/uploads/2012/09/johnny-depp_capt-jack-sparrow1.jpg'
+    }),
+    knex('pirates').insert({
+      name: 'Blackbeard',
+      poison: 'Rum',
+      accessory: 'peg leg',
+      image_url: 'http://www.qaronline.org/portals/3/images/Black%20Beard-Kachik.jpg'
+     })
   );
 };
